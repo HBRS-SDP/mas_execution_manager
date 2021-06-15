8bda14c2a68b98eac6814a2f7f31bcccde44dc21
Michal <mlk04@wp.pl>
2021-06-15 09:51:54 +0200

    [StreamRGBDAction] Added config and json schema

--- test/minimal_working_example/action_states.py
+++ test/minimal_working_example/action_states.py
@@ -4,6 +4,7 @@ from pyftsm.ftsm import FTSMTransitions
 from component_sm import ComponentSM
 from sensor_msgs.msg import PointCloud2
 from kafka import KafkaConsumer
+from jsonschema import validate, ValidationError
 import json
 import math
 import numpy as np
@@ -11,29 +12,36 @@ import numpy as np
 
 class StreamRGBDSM(ComponentSM):
     def __init__(self, 
-                 timeout=5.0, 
-                 input_pointcloud_topic='/hsrb/head_rgbd_sensor/depth_registered/points', 
-                 output_pointcloud_topic='/hsrb/head_rgbd_sensor/pointcloud',
-                 input_event_topic='hsrb_monitoring_rgbd',
+                 component_id,
+                 data_input_topics,
+                 data_output_topics,
+                 monitoring_pipeline_topic,
+                 monitoring_pipeline_server,
+                 data_transfer_timeout,
+                 monitoring_message_schema,
+                 general_message_format,
                  max_recovery_attempts=1):
         
         super(StreamRGBDSM, self).__init__('StreamRGBD', [], max_recovery_attempts)
         
-        self.timeout = timeout
+        self.timeout = data_transfer_timeout
         self.pointcloud = None
+        self.monitoring_message_schema = monitoring_message_schema
         
         # Kafka event listener
-        self.event_listener = KafkaConsumer(input_event_topic,
-                                            value_deserializer=lambda m: json.loads(m.decode('utf-8')),
-                                            consumer_timeout_ms=3000)
+        self.event_listener = \
+        KafkaConsumer(monitoring_pipeline_topic, 
+                      bootstrap_servers=monitoring_pipeline_server,
+                      value_deserializer=lambda m: json.loads(m.decode('utf-8')),
+                      consumer_timeout_ms=3000)
 
         # ROS poincloud listener
-        self.pointcloud_listener = rospy.Subscriber(input_pointcloud_topic, 
+        self.pointcloud_listener = rospy.Subscriber(data_input_topics, 
                                                     PointCloud2,
                                                     self._callback)
 
         # ROS pointcloud publisher
-        self.pointcloud_publisher = rospy.Publisher(output_pointcloud_topic, 
+        self.pointcloud_publisher = rospy.Publisher(data_output_topics, 
                                                     PointCloud2, 
                                                     queue_size=10)
 
@@ -58,22 +66,29 @@ class StreamRGBDSM(ComponentSM):
             time_now = rospy.Time.now()
              
             last_message = None
-            for message in self.event_listener:
+            try:
 
-                last_message = message
+                for message in self.event_listener:
+                    
+                    validate(instance=message.value, schema=self.monitoring_message_schema)
+                    
+                    last_message = message
 
-                time_now = rospy.Time.now()
-                if not time_now - rospy.Duration(self.timeout) < self.last_active_time:
-                    break
+                    time_now = rospy.Time.now()
+                    if not time_now - rospy.Duration(self.timeout) < self.last_active_time:
+                        break
 
-                if message.value['healthStatus']['nans']:
-                    rospy.logerr('Received poincloud conatins to many NaN values.')
-                    return FTSMTransitions.RECOVER
-                else:
-                    rospy.loginfo('Received poincloud conatins acceptable number of NaN values.')
-            
-            if last_message is None:
-                rospy.logwarn('No feedback from the monitor.')
+                    if message.value['healthStatus']['nans']:
+                        rospy.logerr('Received poincloud conatins to many NaN values.')
+                        return FTSMTransitions.RECOVER
+                    else:
+                        rospy.loginfo('Received poincloud conatins acceptable number of NaN values.')
+                
+                if last_message is None:
+                    rospy.logwarn('No feedback from the monitor.')
+
+            except ValidationError:
+                rospy.logwarn('Invalid format of the feedback message from the monitor.')
             
             rospy.sleep(0.1)
             
--- test/minimal_working_example/stream_rgbd_action.py
+++ test/minimal_working_example/stream_rgbd_action.py
@@ -1,10 +1,19 @@
 import rospy
 import actionlib
 from action_states import StreamRGBDSM
+import json
+import yaml
 
 if __name__ == '__main__':
     rospy.init_node('stream_pointcloud')
-    stream_pointcloud = StreamRGBDSM()
+    monitoring_message_schema = json.load(open('monitoring.schema'))
+    general_message_format = json.load(open('general.json'))
+    config = yaml.safe_load(open('config.yaml'))
+
+    stream_pointcloud = StreamRGBDSM(**config, 
+    monitoring_message_schema=monitoring_message_schema,
+    general_message_format=general_message_format)
+    
     try:
         stream_pointcloud.run()
         while stream_pointcloud.is_running and not rospy.is_shutdown():
